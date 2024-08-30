c7a3ce3e485076c8109e7ba509bef2741286df1d
JoeZlonicky <joe.zlonicky@gmail.com>
2024-08-30 15:25:27 -0700

    Log out refactor

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,23 +1,14 @@
 <script setup lang="ts">
-import { router } from '@/router';
 import { useAuthStore } from '@/stores/useAuthStore';
 import { storeToRefs } from 'pinia';
-import { computed } from 'vue';
 
 const authStore = useAuthStore();
 const { needsAuthentication } = storeToRefs(authStore);
 
-function logInOrOutPressed() {
-  // Log in should never be pressable because modal will always be showing
-  if (!needsAuthentication.value) {
-    authStore.logOut();
-    router.push('/');
-  }
+function logOut() {
+  authStore.logOut();
+  window.location.href = '/'; // Force refresh even if on home page
 }
-
-const logInOrOutText = computed(() =>
-  needsAuthentication.value ? 'Log in' : 'Log out',
-);
 </script>
 <template>
   <header class="flex flex-wrap bg-primary text-3xl">
@@ -25,19 +16,20 @@ const logInOrOutText = computed(() =>
       <span class="font-bold text-accent">TheBlog&nbsp;Admin</span
       ><span class="text-lg"> By&nbsp;Joe&nbsp;Zlonicky</span>
     </div>
-    <nav v-else>
+    <nav v-else class="flex-1 border-r-2 border-accent border-opacity-50">
       <RouterLink
         to="/"
-        class="block flex-1 px-8 py-4 font-bold text-accent hover:bg-primary-highlight"
+        class="block px-8 py-4 font-bold text-accent hover:bg-primary-highlight"
       >
         &larr; Dashboard
       </RouterLink>
     </nav>
     <button
+      v-show="!needsAuthentication"
       class="mx-4 my-auto max-h-12 rounded-md text-lg"
-      @click="logInOrOutPressed"
+      @click="logOut"
     >
-      {{ logInOrOutText }}
+      Log out
     </button>
   </header>
 </template>
